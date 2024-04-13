856e1222fbb041589120a2705b62cc121e48946b
Anonymous Proxy <53960525+YYHEggEgg@users.noreply.github.com>
2024-04-13 16:16:47 +0800

    Recover sample index.py & matrix strategy for workflow

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 def handleRequest(request):
-    return __new__(Response("Python Worker hello world!"))
+    return __new__(Response('Python Worker hello world!', {
+        'headers' : { 'content-type' : 'text/plain' }
+    }))
 
-addEventListener("fetch", (lambda event: event.respondWith(handleRequest(event.request))))
+addEventListener('fetch', (lambda event: event.respondWith(handleRequest(event.request))))
